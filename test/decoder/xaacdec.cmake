977a5c4a11452a4a19c093bf07140eabbb86d57a
Bohdan Bolbot <bohdan.bolbot@globallogic.com>
2026-01-30 23:25:12 +0200

    rectify _CRT_SECURE_NO_WARNINGS

--- test/decoder/xaacdec.cmake
+++ test/decoder/xaacdec.cmake
@@ -34,23 +34,19 @@ if(NOT RC_FALLBACK)
         target_link_libraries(xaacdec kernel32 ws2_32 ntdll userenv dbghelp)
     elseif(APPLE)
         target_link_libraries(xaacdec pthread)
-        set_target_properties(xaacdec PROPERTIES 
-            LINK_FLAGS 
+        set_target_properties(xaacdec PROPERTIES
+            LINK_FLAGS
             "-framework Security -framework CoreFoundation")
     elseif(UNIX)
         target_link_libraries(xaacdec pthread dl m)
     endif()
 endif()
 
-if(WIN32)
-    target_compile_definitions(xaacdec PRIVATE
-        _CRT_SECURE_NO_WARNINGS)
-endif()
-
 target_compile_definitions(xaacdec PRIVATE
     DRC_ENABLE MULTICHANNEL_ENABLE ECLIPSE LOUDNESS_LEVELING_SUPPORT
-    WIN32) # TODO: fix WIN32 diverging from _WIN32
+    WIN32 # TODO: fix WIN32 diverging from _WIN32
+    $<$<C_COMPILER_FRONTEND_VARIANT:MSVC>:_CRT_SECURE_NO_WARNINGS>)
 
 target_compile_options(xaacdec PRIVATE
-    $<$<C_COMPILER_ID:MSVC>:/UARM_PROFILE_HW;/UARM_PROFILE_BOARD>
-    $<$<NOT:$<C_COMPILER_ID:MSVC>>:-UARM_PROFILE_HW;-UARM_PROFILE_BOARD>)
+    $<$<C_COMPILER_FRONTEND_VARIANT:MSVC>:/UARM_PROFILE_HW;/UARM_PROFILE_BOARD>
+    $<$<NOT:$<C_COMPILER_FRONTEND_VARIANT:MSVC>>:-UARM_PROFILE_HW;-UARM_PROFILE_BOARD>)
