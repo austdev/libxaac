86a63e99f36883acf681ec05797283a92f1cd43d
Oleksandr Movchan <oleksandr.v.movchan@globallogic.com>
2026-02-10 16:59:17 +0200

    Trigger cmake build automatically in rust/build.rs

--- rust/build.rs
+++ rust/build.rs
@@ -1,24 +1,16 @@
-use std::env;
-
 fn main() {
-    // Configuration type:
-    let config = env::var("PROFILE").unwrap();
-    let config = if config == "release" { "Release" } else { "Debug" };
-
-    println!("cargo:rustc-link-search=native=../build/{}", config);
-    println!("cargo:rustc-link-lib=dylib=xaacdec-ref");
-
-    // Make the binary find the .so at runtime without LD_LIBRARY_PATH:
-    #[cfg(target_os = "linux")]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
-
-    // Make the binary find the .dylib at runtime without DYLD_LIBRARY_PATH:
-    #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path");
+    let mut cmake_cfg =  cmake::Config::new("..");
+    cmake_cfg.build_target("libxaacdec");
+    if cfg!(target_os = "linux") && std::process::Command::new("ninja").arg("--version").spawn().is_ok() {
+        cmake_cfg.generator("Ninja");
+    }
+    let out_path = cmake_cfg.build();
+    println!("cargo:warning={}", out_path.display());
+    println!("cargo:rustc-link-search=native={}/build", out_path.display());
+    println!("cargo:rustc-link-lib=static=xaacdec-ref");
 
     // Link definitions of `printf` and other legacy functions that are not in the msvcrt
     if std::env::var("CARGO_CFG_TARGET_ENV").as_deref() == Ok("msvc") {
         println!("cargo:rustc-link-lib=legacy_stdio_definitions");
     }
-
 }
