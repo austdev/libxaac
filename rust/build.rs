2a14a007fe9c8cba6fc4017c2686fbd51cf44cc4
Oleksandr Movchan <oleksandr.v.movchan@globallogic.com>
2026-02-11 12:34:26 +0200

    Setup cmake toolchains and presets

--- rust/build.rs
+++ rust/build.rs
@@ -1,16 +1,32 @@
 fn main() {
-    let mut cmake_cfg =  cmake::Config::new("..");
+    println!("cargo::rerun-if-env-changed=CMAKE_TOOLCHAIN_FILE");
+
+    let mut cmake_cfg = cmake::Config::new("..");
+
     cmake_cfg.build_target("libxaacdec");
-    if cfg!(target_os = "linux") && std::process::Command::new("ninja").arg("--version").spawn().is_ok() {
+
+    if let Some(toolchain_file) = option_env!("CMAKE_TOOLCHAIN_FILE") {
+        cmake_cfg.define("CMAKE_TOOLCHAIN_FILE", toolchain_file);
+    }
+
+    if cfg!(target_os = "linux")
+        && std::process::Command::new("ninja")
+            .arg("--version")
+            .spawn()
+            .is_ok()
+    {
         cmake_cfg.generator("Ninja");
     }
+
     let out_path = cmake_cfg.build();
-    println!("cargo:warning={}", out_path.display());
-    println!("cargo:rustc-link-search=native={}/build", out_path.display());
-    println!("cargo:rustc-link-lib=static=xaacdec-ref");
+    println!(
+        "cargo::rustc-link-search=native={}/build",
+        out_path.display()
+    );
+    println!("cargo::rustc-link-lib=static=xaacdec-ref");
 
     // Link definitions of `printf` and other legacy functions that are not in the msvcrt
     if std::env::var("CARGO_CFG_TARGET_ENV").as_deref() == Ok("msvc") {
-        println!("cargo:rustc-link-lib=legacy_stdio_definitions");
+        println!("cargo::rustc-link-lib=legacy_stdio_definitions");
     }
 }
